bc77771fe209d97c97e8ece36b0688b48bed3a69
TheMartin <64.Martin@gmail.com>
2017-01-08 14:23:20 +0100

    Initial project structure

---
+++ config/systemjs.config.js
@@ -0,0 +1,20 @@
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: '.',
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './test/main.js',
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
