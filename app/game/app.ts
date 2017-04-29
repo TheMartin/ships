2481d62cb3b042397808fd4c436610a7e9742363
TheMartin <64.Martin@gmail.com>
2017-04-30 00:49:14 +0200

    Game title screen, fullscreen

---
+++ app/game/app.ts
@@ -0,0 +1,93 @@
+import { Game } from "../game/game";
+import { Renderer } from "../renderer/renderer";
+import * as Fullscreen from "../util/fullscreen";
+import { VdomElement, createElement } from "../vdom/vdom";
+
+export class App
+{
+  private constructor(private $root : HTMLElement)
+  {
+    this.$root.appendChild(createElement(
+      VdomElement.create("ul", {"class" : "menu"},
+
+        VdomElement.create("li", {},
+          VdomElement.create("a", {
+            "href" : "",
+            "class" : "btn",
+            "onclick" : (e : Event) =>
+            {
+              this.runGame(false);
+              e.preventDefault();
+            }
+          },
+          "Start")
+        ),
+
+        VdomElement.create("li", {},
+          VdomElement.create("a", {
+            "href" : "",
+            "class" : "btn",
+            "onclick" : (e : Event) =>
+            {
+              this.runGame(true);
+              e.preventDefault();
+            }
+          },
+          "Start fullscreen")
+        )
+      )
+    ));
+
+    this.$root.appendChild(createElement(
+      VdomElement.create("div", {"class" : "title"}, "Ships")
+    ));
+  }
+
+  static mount($root : HTMLElement) : App
+  {
+    $root.addEventListener("contextmenu", (e : Event) => { e.preventDefault(); });
+    return new App($root);
+  }
+
+  runGame(fullscreen : boolean) : void
+  {
+    let canvas = document.createElement("canvas");
+
+    if (Fullscreen.fullscreenAvailable())
+    {
+      if (fullscreen)
+        Fullscreen.setFullscreen(this.$root);
+
+      document.addEventListener("keydown", (e : KeyboardEvent) =>
+      {
+        if (e.keyCode !== 13)
+          return;
+
+        if (Fullscreen.isFullscreen())
+        {
+          Fullscreen.disableFullscreen();
+        }
+        else
+        {
+          Fullscreen.setFullscreen(this.$root);
+        }
+      });
+    }
+
+    let resize = () =>
+    {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+    resize();
+    window.addEventListener("resize", resize);
+
+    while (this.$root.firstChild)
+      this.$root.removeChild(this.$root.firstChild);
+
+    this.$root.appendChild(canvas);
+    let renderer = new Renderer(canvas);
+    let game = new Game(this.$root, canvas, renderer);
+    game.start(60);
+  }
+};
--- app/main.ts
+++ app/main.ts
@@ -1,37 +1,4 @@
-import { Game } from "./game/game";
-import { Renderer } from "./renderer/renderer";
-import { UiManager } from "./ui/uiManager";
-import * as Fullscreen from "./util/fullscreen";
+import { App } from "./game/app";
 import "./styles/styles.css";
 
-let content = document.getElementById("content");
-let canvas = document.createElement("canvas");
-content.addEventListener("contextmenu", (e : Event) => { e.preventDefault(); });
-let resize = () =>
-{
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-};
-resize();
-window.addEventListener("resize", resize);
-if (Fullscreen.fullscreenAvailable())
-{
-  document.addEventListener("keydown", (e : KeyboardEvent) =>
-  {
-    if (e.keyCode !== 13)
-      return;
-
-    if (Fullscreen.isFullscreen())
-    {
-      Fullscreen.disableFullscreen();
-    }
-    else
-    {
-      Fullscreen.setFullscreen(content);
-    }
-  });  
-}
-content.appendChild(canvas);
-let renderer = new Renderer(canvas);
-let game = new Game(content, canvas, renderer);
-game.start(60);
+App.mount(document.getElementById("content"));
